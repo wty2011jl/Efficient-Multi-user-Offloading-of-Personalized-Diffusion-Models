8e3f8429a274e76b50c3d35ee0593f13fd57a9f3
wty2011jl <145192003+wty2011jl@users.noreply.github.com>
2025-02-22 20:44:34 +0800

    Update point_generation.py

--- fitting/point_generation.py
+++ fitting/point_generation.py
@@ -14,7 +14,7 @@ import math
 import numpy as np
 
 prompt1 = "A photo of sks dog in the box"  #0-20
-prompt2 = "A photo of sks dog running on the grass"
+prompt2 = "A photo of sks dog on the grass"
 prompt3 = "A photo of sks dog on the sofa"
 prompt4 = "A photo of sks dog next to the car"
 prompt5 = "A photo of sks dog siting on the floor"
